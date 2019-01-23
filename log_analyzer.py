7c75f5077741a4d2138b10e9470dba644321a281
IlyaSleptsov <ilya1242@gmail.com>
2019-01-23 23:44:37 +0300

    some editions

--- log_analyzer.py
+++ log_analyzer.py
@@ -13,7 +13,6 @@ import logging
 import datetime
 import re
 import collections
-import numpy as np
 import operator
 import json
 import string
@@ -29,15 +28,26 @@ config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log",
-    "FILE_FOR_LOGS": "logs"
+    "LOG_FILE": "logs",
+    "REGEX_1": r"^nginx-access-ui\.log-(\d{8})(\.gz)?$",
+    "REGEX_2": r"nginx-access-ui.log-\n|\d{8}"
 }
 
-regex_for_log_file = re.compile('nginx-access-ui.log-\n|\d{8}')
+
+def count_median(lst):
+    """Computes median of list"""
+    n = len(lst)
+    if n < 1:
+        return None
+    if n % 2 == 1:
+        return sorted(lst)[n//2]
+    else:
+        return sum(sorted(lst)[n//2-1:n//2+1])/2.0
 
 
-def get_date_from_file_name(file_name):
+def get_date_from_file_name(file_name, regex=re.compile(config['REGEX_2'])):
     """Finds date in filename string and transforms it to datetime"""
-    return datetime.datetime.strptime(re.search(regex_for_log_file, file_name)[0], '%Y%m%d')
+    return datetime.datetime.strptime(re.search(regex, file_name)[0], '%Y%m%d')
 
 
 def get_dict_from_config(config_path):
@@ -47,19 +57,15 @@ def get_dict_from_config(config_path):
     return dict((name.upper(), value) for (name, value) in cfg.items('default'))
 
 
-def get_last_log_file(path_to_file):
+def get_last_log_file(path_to_file,regex=re.compile(config['REGEX_1'])):
     """Finds last log file by date"""
     last_file = {'file_name': 'nothing', 'date': datetime.datetime(2001, 1, 1)}
     for file_name in os.listdir(path_to_file):
-        try:
-            if re.search(regex_for_log_file, file_name):
-                log_date = get_date_from_file_name(file_name)
-                if log_date > last_file['date']:
-                    last_file['file_name'] = os.path.join(path_to_file, file_name)
-                    last_file['date'] = log_date
-        except Exception as e:
-            logging.info('Problem {} with file {}'.format(e, file_name))
-
+        if re.search(regex, file_name):
+            log_date = get_date_from_file_name(file_name)
+            if log_date > last_file['date']:
+                last_file['file_name'] = os.path.join(path_to_file, file_name)
+                last_file['date'] = log_date
     return last_file
 
 
@@ -69,32 +75,35 @@ def parse_log_files(log_file):
         log = gzip.open(log_file, 'rb')
     else:
         log = open(log_file)
-    for line in log:
-        yield line.decode()
+    with log:
+        for line in log:
+            yield line.decode()
     log.close()
 
 
 def get_url_and_time(line):
+    """Extracts time and url from line of log file"""
     list_of_values = line.split(' ')
-    try:
-        url = list_of_values[7].strip()
-        time = float(list_of_values[-1].strip())
-    except Exception:
-        return None, None
+    url = list_of_values[7].strip()
+    time = float(list_of_values[-1].strip())
     return url, time
 
 
-def extract_line_from_log_file(log_file):
+def extract_line_from_log_file(log_file,tresh=0.8):
     """Extracts information from log file lines """
     url_stats = collections.defaultdict()
     sum_urls = 0
     sum_time = 0
+    sum_all = 0
     for line in log_file:
         url, time = get_url_and_time(line)
         if url and time:
             sum_urls += 1
             sum_time += time
             url_stats[url] = url_stats.get(url, list()) + [time]
+        sum_all += 1
+    if sum_urls / sum_all < tresh:
+        raise Exception("There are too many mistakes in log file line. More than {}%".format(100-tresh*100))
     result = []
     for url, time in url_stats.items():
         result.append({
@@ -103,11 +112,11 @@ def extract_line_from_log_file(log_file):
             'count_perc': round(len(time) / sum_urls, 2),
             'time_sum': sum(time),
             'time_perc': round(sum(time) / sum_time, 2),
-            'time_avg': round(np.mean(time), 2),
+            'time_avg': round(sum(time)/len(time), 2),
             'time_max': max(time),
-            'time_med': np.median(time)
+            'time_med': count_median(time)
         })
-    result.sort(key=operator.itemgetter('count'), reverse=True)
+    result.sort(key=operator.itemgetter('time_sum'), reverse=True)
     return result
 
 
@@ -126,20 +135,20 @@ def main(config):
     if os.path.isfile(last_file['file_name']):
         report_file = os.path.join(config['REPORT_DIR'],
                                    'report_{}.html'.format(last_file['date'].strftime('%Y-%m-%d')))
+        report_file_pd = os.path.join(config['REPORT_DIR'],
+                                   'report_pd_{}.html'.format(last_file['date'].strftime('%Y-%m-%d')))
         if not os.path.exists(report_file):
             result = extract_line_from_log_file(parse_log_files(last_file['file_name']))
             if result:
                 make_report(result[0:config['REPORT_SIZE']], report_file)
-                pd.DataFrame(result[0:config['REPORT_SIZE']]).to_html(report_file)
+                pd.DataFrame(result[0:config['REPORT_SIZE']]).to_html('{}'.format(report_file_pd))
                 logging.info('Report is created')
             else:
                 logging.info('Something went wrong with extracting lines')
         else:
-            logging.info('Last logfile is already analyzed')
+            logging.info('{} logfile is already analyzed'.format(report_file))
     else:
         logging.info('Program did not find relevant log files')
-
-    print(last_file)
 
 
 if __name__ == "__main__":
@@ -153,10 +162,12 @@ if __name__ == "__main__":
 
         logging.basicConfig(format=u'[%(asctime)s] %(levelname).1s %(message)s',
                             datefmt='%Y.%m.%d %H:%M:%S',
-                            filename=config.get('FILE_FOR_LOGS'),
+                            filename=config.get('LOG_FILE'),
                             filemode='a',
                             level=logging.INFO)
-
-        main(config)
+        try:
+            main(config)
+        except Exception as e:
+            logging.exception(e)
     else:
         raise NameError('This {} config file is not correct'.format(args.config))
--- test_log_analyzer.py
+++ test_log_analyzer.py
@@ -4,7 +4,10 @@ import datetime
 
 class LogAnalyzerTests(unittest.TestCase):
     def test_get_date_from_file_name(self):
-        self.assertEqual(log_analyzer.get_date_from_file_name('nginx-access-ui.log-20170630.gz'),datetime.datetime(2017,6,30))
+        self.assertEqual(log_analyzer.get_date_from_file_name('nginx-access-ui.log-20170630.gz') ,datetime.datetime(2017,6,30))
+    def test_median(self):
+        self.assertEqual(log_analyzer.count_median([1,1,2,3,3]),2)
+
 
 if __name__ == '__main__':
     unittest.main()
