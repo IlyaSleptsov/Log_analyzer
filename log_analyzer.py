7234a5fdcf1759688be261e71b0e121689f06999
IlyaSleptsov <ilya1242@gmail.com>
2019-02-12 20:30:37 +0300

    tests and report improvements

--- log_analyzer.py
+++ log_analyzer.py
@@ -17,7 +17,7 @@ import operator
 import json
 import string
 import argparse
-import pandas as pd
+from collections import namedtuple
 
 try:
     import configparser
@@ -29,8 +29,8 @@ config = {
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log",
     "LOG_FILE": "logs",
-    "REGEX_1": r"^nginx-access-ui\.log-(\d{8})(\.gz)?$",
-    "REGEX_2": r"nginx-access-ui.log-\n|\d{8}"
+    "REGEX": r"^nginx-access-ui\.log-(\d{8})(\.gz)?$",
+    "TEMPLATE_FILE": 'report.html'
 }
 
 
@@ -45,11 +45,6 @@ def count_median(lst):
         return sum(sorted(lst)[n//2-1:n//2+1])/2.0
 
 
-def get_date_from_file_name(file_name, regex=re.compile(config['REGEX_2'])):
-    """Finds date in filename string and transforms it to datetime"""
-    return datetime.datetime.strptime(re.search(regex, file_name)[0], '%Y%m%d')
-
-
 def get_dict_from_config(config_path):
     """Parses config file and transferts it into dict"""
     cfg = configparser.RawConfigParser(allow_no_value=True)
@@ -57,16 +52,22 @@ def get_dict_from_config(config_path):
     return dict((name.upper(), value) for (name, value) in cfg.items('default'))
 
 
-def get_last_log_file(path_to_file,regex=re.compile(config['REGEX_1'])):
+def get_last_log_file(path_to_file, regex=re.compile(config['REGEX'])):
     """Finds last log file by date"""
-    last_file = {'file_name': 'nothing', 'date': datetime.datetime(2001, 1, 1)}
+    last_file = namedtuple('file', ['name', 'date'])
+    log_name = log_date = None
     for file_name in os.listdir(path_to_file):
-        if re.search(regex, file_name):
-            log_date = get_date_from_file_name(file_name)
-            if log_date > last_file['date']:
-                last_file['file_name'] = os.path.join(path_to_file, file_name)
-                last_file['date'] = log_date
-    return last_file
+        match = re.search(regex, file_name)
+        if match:
+            date, ext = match.groups()
+            current_file_date = datetime.datetime.strptime(date, '%Y%m%d')
+            if log_name is None:
+                log_name = os.path.join(path_to_file, file_name)
+                log_date = current_file_date
+            if current_file_date > log_date:
+                log_name = os.path.join(path_to_file, file_name)
+                log_date = current_file_date
+    return last_file(name=log_name, date=log_date)
 
 
 def parse_log_files(log_file):
@@ -77,15 +78,21 @@ def parse_log_files(log_file):
         log = open(log_file)
     with log:
         for line in log:
-            yield line.decode()
-    log.close()
+            try:
+                yield line.decode()
+            except AttributeError:
+                yield line
 
 
 def get_url_and_time(line):
     """Extracts time and url from line of log file"""
-    list_of_values = line.split(' ')
-    url = list_of_values[7].strip()
-    time = float(list_of_values[-1].strip())
+    try:
+        list_of_values = line.split(' ')
+        url = list_of_values[7].strip()
+        time = float(list_of_values[-1].strip())
+    except Exception:
+        return None, None
+
     return url, time
 
 
@@ -103,7 +110,7 @@ def extract_line_from_log_file(log_file,tresh=0.8):
             url_stats[url] = url_stats.get(url, list()) + [time]
         sum_all += 1
     if sum_urls / sum_all < tresh:
-        raise Exception("There are too many mistakes in log file line. More than {}%".format(100-tresh*100))
+        raise Exception("There are too many mistakes in log file line")
     result = []
     for url, time in url_stats.items():
         result.append({
@@ -120,9 +127,9 @@ def extract_line_from_log_file(log_file,tresh=0.8):
     return result
 
 
-def make_report(data_to_report, file_name):
+def make_report_1(data_to_report, file_name, template_file):
     """Generates report"""
-    template_report = string.Template('$json_table').substitute(json_table=json.dumps(data_to_report))
+    template_report = string.Template('$json_table').substitute(json_table = json.dumps(data_to_report))
     try:
         with open(file_name, 'w') as f:
             f.write(template_report)
@@ -130,18 +137,26 @@ def make_report(data_to_report, file_name):
         logging.info('Problem with writing file: {}'.format(e))
 
 
+def make_report(data_to_report, file_name, template_file):
+    with open(template_file, 'r') as tf:
+        template = tf.read()
+    template = template.replace('$table_json', json.dumps(data_to_report))
+    try:
+        with open(file_name, 'w') as f:
+            f.write(template)
+    except Exception as e:
+        logging.info('Problem with writing file: {}'.format(e))
+
+
 def main(config):
     last_file = get_last_log_file(config['LOG_DIR'])
-    if os.path.isfile(last_file['file_name']):
+    if os.path.isfile(last_file.name):
         report_file = os.path.join(config['REPORT_DIR'],
-                                   'report_{}.html'.format(last_file['date'].strftime('%Y-%m-%d')))
-        report_file_pd = os.path.join(config['REPORT_DIR'],
-                                   'report_pd_{}.html'.format(last_file['date'].strftime('%Y-%m-%d')))
+                                   'report_{}.html'.format(last_file.date.strftime('%Y-%m-%d')))
         if not os.path.exists(report_file):
-            result = extract_line_from_log_file(parse_log_files(last_file['file_name']))
+            result = extract_line_from_log_file(parse_log_files(last_file.name))
             if result:
-                make_report(result[0:config['REPORT_SIZE']], report_file)
-                pd.DataFrame(result[0:config['REPORT_SIZE']]).to_html('{}'.format(report_file_pd))
+                make_report(result[0:config['REPORT_SIZE']], report_file, config['TEMPLATE_FILE'])
                 logging.info('Report is created')
             else:
                 logging.info('Something went wrong with extracting lines')
--- test_log_analyzer.py
+++ test_log_analyzer.py
@@ -1,12 +1,62 @@
 import unittest
+from unittest import mock
 import log_analyzer
 import datetime
+import os
+import collections
+import operator
+
+LOG_LINES = [
+    '1.196.116.32 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/banner/25019354 HTTP/1.1" 200 927 "-" "Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5" "-" "1498697422-2190034393-4708-9752759" "dc7161be3" 0.390',
+    '1.99.174.176 3b81f63526fa8  - [29/Jun/2017:03:50:22 +0300] "GET /api/1/photogenic_banners/list/?server_name=WIN7RB4 HTTP/1.1" 200 12 "-" "Python-urllib/2.7" "-" "1498697422-32900793-4708-9752770" "-" 0.133',
+    '1.169.137.128 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/banner/16852664 HTTP/1.1" 200 19415 "-" "Slotovod" "-" "1498697422-2118016444-4708-9752769" "712e90144abee9" 0.199',
+    '1.199.4.96 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/slot/4705/groups HTTP/1.1" 200 2613 "-" "Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5" "-" "1498697422-3800516057-4708-9752745" "2a828197ae235b0b3cb" 0.704',
+    '1.168.65.96 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/internal/banner/24294027/info HTTP/1.1" 200 407 "-" "-" "-" "1498697422-2539198130-4709-9928846" "89f7f1be37d" 0.146']
+BAD_LOG_LINES = [
+    '1.196.116.32 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/banner/25019354 HTTP/1.1" 200 927 "-" "Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5" "-" "1498697422-2190034393-4708-9752759" "dc7161be3" 0.390',
+    'bad_line',
+    'another_bad_line'
+]
+
 
 class LogAnalyzerTests(unittest.TestCase):
-    def test_get_date_from_file_name(self):
-        self.assertEqual(log_analyzer.get_date_from_file_name('nginx-access-ui.log-20170630.gz') ,datetime.datetime(2017,6,30))
+    def setUp(self):
+        self.config = {
+            "REPORT_SIZE": 1000,
+            "REPORT_DIR": "./reports",
+            "LOG_DIR": "./log",
+            "LOG_FILE": "logs",
+            "REGEX": r"^nginx-access-ui\.log-(\d{8})(\.gz)?$",
+        }
+
     def test_median(self):
-        self.assertEqual(log_analyzer.count_median([1,1,2,3,3]),2)
+        self.assertEqual(log_analyzer.count_median([1, 1, 2, 3, 3]), 2)
+
+    @mock.patch.object(os, 'listdir')
+    def test_get_last_log_file(self, mock_listdir):
+        mock_listdir.return_value = ['nginx-access-ui.log-20170630',
+                                     'nginx-access-ui.log-20170629',
+                                     'nginx-access-ui.log-20160730']
+        last_file = log_analyzer.get_last_log_file(self.config['LOG_DIR'])
+        self.assertEqual(last_file.date, datetime.datetime(2017, 6, 30))
+
+    @mock.patch.object(os, 'listdir')
+    def test_finds_correct_file(self, mock_listdir):
+        mock_listdir.return_value = ['something_wrong_20170630',
+                                     'nginx-access-ui.log-20170630.gz',
+                                     'nginx-access-ui.log-20160730.py',
+                                     'nginx-access-ui.log-20160730.something_wrong_again']
+        last_file = log_analyzer.get_last_log_file(self.config['LOG_DIR'])
+        self.assertEqual(last_file.name, self.config['LOG_DIR'] + '\\' + 'nginx-access-ui.log-20170630.gz')
+
+    def test_extract_line_from_log_file(self):
+        results = log_analyzer.extract_line_from_log_file(LOG_LINES)
+        self.assertGreater(len(results), 0)
+
+    def test_extract_line_threshold(self):
+        with self.assertRaises(Exception) as context:
+            log_analyzer.extract_line_from_log_file(BAD_LOG_LINES)
+            self.assertTrue("There are too many mistakes in log file line" in context.exception)
 
 
 if __name__ == '__main__':
